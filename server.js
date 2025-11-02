d8920a0b74f4e39a34a8bfac3fb9fa9792d2f15d
whmccoy <whmccoy@gmail.com>
2025-11-02 10:13:00 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+// CS 248 hello world server
+const express = require("express");
+const app = express();
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send('Hello World!');
+})
+app.listen(3000);
